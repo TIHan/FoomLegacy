d7f5b4bf4fcdf3ea0ed635096495eeeabcc2f0c0
Will Smith <lol.tihan@gmail.com>
2015-01-02 01:01:54 -0800

    Forgot to add files

---
+++ src/Foom.Shared/earClipping.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Foom.Shared.Triangulation.EarClipping
+
+open System.Numerics
+
+open Foom.Shared
+
+let compute (poly: Polygon) = List.empty<Vector2 []>
---
+++ src/Foom.Shared/earClipping.fsi
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Foom.Shared.Triangulation.EarClipping
+
+open System.Numerics
+
+open Foom.Shared
+
+val compute : Polygon -> Vector2 [] list
