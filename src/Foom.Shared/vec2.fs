227bfbf9a57bfa0d5fab7bf747a82c0e22a8f0ef
Will Smith <lol.tihan@gmail.com>
2015-03-06 21:39:15 -0800

    Deleted vec2.fs

--- src/Foom.Shared/vec2.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace Foom.Shared.Numerics
-
-open System
-open System.Numerics
-
-[<RequireQualifiedAccess>]
-module Vec2 =
-    let angle (v: Vector2) =
-        -(single <| atan2 (float -v.Y) (float v.X)) + single Math.PI * 0.5f
